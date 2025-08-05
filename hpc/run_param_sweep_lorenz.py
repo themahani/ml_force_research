8ba2cd0512619d26c0b1ab1e3412671a587d92e5
Ali Mahani <ali.a.mahani@zoho.com>
2025-08-05 11:35:40 -0600

    fix: error JSON encoder not parsing class names

--- hpc/run_param_sweep_lorenz.py
+++ hpc/run_param_sweep_lorenz.py
@@ -4,20 +4,24 @@
 This script runs a parameter sweep for the Lorenz system using the ml-force package.
 """
 
-import torch
-import numpy as np
 import json
+import os
+import sys
 
-import sys, os
+import numpy as np
+import torch
+
 sys.path.append(os.path.join(os.getcwd(), ".."))
 
-from ml_force import LorenzAttractor, MorrisLecarCurrent, minmax_transform
-from ml_force.optimizers import BruteForceMesh, NumpyArrayEncoder
+from rctorch.models import MorrisLecarCurrent
+from rctorch.optimizers import BruteForceMesh, NumpyArrayEncoder
+from rctorch.supervisors import LorenzAttractor
+from rctorch.utils import minmax_transform
 
 
 def main():
     # Define the output directory
-    output_dir = os.path.join(os.getcwd(), "bfm_output", "lorenz_current_focused1")
+    output_dir = os.path.join(os.getcwd(), "bfm_output", "lorenz_current_grand")
     # Define the directory to save results
     os.makedirs(output_dir, exist_ok=True)
 
@@ -27,45 +31,47 @@ def main():
     torch.random.manual_seed(seed)
 
     # Generate the supervisor signal
-    T = 25_000
-    dt = 5e-2
-    # t = np.arange(0, T, dt)
-    x = LorenzAttractor(T, dt, tau=.01).generate(transient_time=500.0)
+    T = 20_000
+    dt = 1e-1
+    x = LorenzAttractor(T, dt, tau=0.01).generate(transient_time=500.0)
 
     x = x.T
     sup = minmax_transform(x, zero_mean=True)
     print(sup.shape)
     np.save(os.path.join(output_dir, "supervisor.npy"), sup)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    sup_tensor = torch.tensor(sup, device=device, dtype=torch.float32)
 
     # Set the model parameters
-    N = 10_000
-    BIAS = np.ones((N, 1)) * 75.0
-    model_params = {
-        "N": N,
-        "T": T,
+    Ne = 4_000
+    Ni = 1_000
+    N = Ne + Ni
+    BIAS = np.ones((N, 1)) * 65.0
+    reservoir_params = {
+        "model_cls": MorrisLecarCurrent,
+        "n_input": sup_tensor.size(1),
+        "n_output": sup_tensor.size(1),
+        "Ne": Ne,
+        "Ni": Ni,
         "dt": dt,
-        "supervisor": sup,
         "BIAS": BIAS,
         "p_sparsity": 0.1,
-        "l":8e-1,
-        "device": torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
+        "device": torch.device("cuda" if torch.cuda.is_available() else "cpu"),
     }
 
     try:
-        params_file = os.path.join(output_dir, "model_params.json")
+        params_file = os.path.join(output_dir, "reservoir_params.json")
         with open(params_file, "w") as f:
-            json.dump(model_params, f, cls=NumpyArrayEncoder, indent=4) # Added indent for readability
+            json.dump(
+                reservoir_params, f, cls=NumpyArrayEncoder, indent=4
+            )  # Added indent for readability
     except Exception as e:
         print(f"Error exporting JSON file params: {e}")
 
-    Q_range = np.linspace(250, 350, 3)
-    gbar_range = np.linspace(15, 25, 5)
+    q_range = np.linspace(20, 450, 10)
+    gbar_range = np.linspace(5, 40, 7)
 
-
-    opt_params = {
-        "Q": Q_range,
-        "gbar": gbar_range
-    }
+    opt_params = {"w_in_amp": q_range, "gbar": gbar_range}
 
     size = 1
     for _, value_range in opt_params.items():
@@ -73,20 +79,35 @@ def main():
 
     print(f"total size of the mesh is: {size}")
 
-    render_params = dict(rls_start=500, 
-                         rls_stop=T-10_000, rls_step=20,
-                         live_plot=False, plt_interval=300,
-                         n_neurons=10, save_all=False)
-    
+    train_test_split = 0.5
+    nt_split = int(sup_tensor.size(0) * train_test_split)
+    sup_train = sup_tensor[:nt_split]
+    sup_test = sup_tensor[nt_split:]
+    train_kwargs = dict(
+        x=sup_train,
+        nt_transient=int(500 / dt),
+        rls_step=20,
+        ridge_reg=1.0,
+        ff_coeff=1.0,
+    )
+    test_kwargs = {
+        "x": sup_test,
+        "nt_transient": 0,
+        "closed_loop": True,
+    }
+
     n_threads = int(input("Enter number of threads:\n>>"))
 
-    bfm = BruteForceMesh(model=MorrisLecarCurrent, default_args=model_params,
-                         render_args=render_params, params=opt_params, num_threads=n_threads)
-    
-    
+    bfm = BruteForceMesh(
+        reservoir_kwargs=reservoir_params,
+        train_kwargs=train_kwargs,
+        test_kwargs=test_kwargs,
+        params=opt_params,
+        num_threads=n_threads,
+    )
+
     bfm.run(output_dir)
     print(f"Results saved in: {output_dir}")
-
 
 
 if __name__ == "__main__":
